3c4a802561fae1c26666aa7ab4fc702ce1d51a0a
Uniplac <github@uniplaclages.edu.br>
2022-08-12 17:27:40 -0300

    git

--- NotaFiscal/Program.cs
+++ NotaFiscal/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        decimal valor_total = 0, percentual_impostos = 0, valor_imposto = 0;
+
+        Console.Write("Informe o valor da  nota fiscal:");
+        string valor_nota = Console.ReadLine();
+
+        decimal valor = decimal.Parse(valor_nota);
+        if (valor <= 999)
+        {
+        percentual_impostos = 2;
+        valor_imposto = valor * 0.02M;
+        valor_total = (valor * 0.02M) + valor;
+        }
+
+        else if (valor >= 1000 && valor < 2999)
+        {
+            percentual_impostos = 2.5M;
+            valor_imposto = valor * 0.025M;
+            valor_total = (valor * 0.025M) + valor;
+        }
+        else if (valor >= 3000 && valor < 6999)
+        {
+            percentual_impostos = 2.8M;
+            valor_imposto = valor * 0.028M;
+            valor_total = (valor * 0.028M) + valor;
+        }
+        else
+        {
+            percentual_impostos = 3M;
+            valor_imposto =  valor * 0.03M;
+            valor_total = (valor * 0.03M) + valor;
+        }
+
+        Console.WriteLine($"Valor FINAL da nota fiscal: {valor_total}");
+        Console.WriteLine($"Percentual aplicado  de imposto: {percentual_impostos} %");
+        Console.WriteLine($"Valor do imposto: {valor_imposto}");
+    }
+
+}
+
---
+++ Sorteio/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Random randNum = new Random();
+
+        int valorR = randNum.Next(0, 10);
+        Console.Write("Informe um valor inteiro entre 0 e 10:");
+        String valor = Console.ReadLine();
+
+
+        int resposta = int.Parse(valor);
+        if (resposta == valorR)
+        {
+            Console.WriteLine("Parabéns! Você acertou o número!");
+        }
+        else if (resposta > valorR)
+        {
+            Console.WriteLine("Deveria ser um numero menor!");
+        }
+        else if (resposta < valorR)
+        {
+            Console.WriteLine("Deveria ser um numero maior!");
+        }
+        else
+        {
+            Console.WriteLine("Errou!!!!!");
+        }
+                    Console.WriteLine(valorR);
+    }
+}
