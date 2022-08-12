6dad9886995b7f17e880c2a29f2f7a752b24737e
Uniplac <github@uniplaclages.edu.br>
2022-08-12 18:09:36 -0300

    nvosa

---
+++ NovoSalario/Program.cs
@@ -0,0 +1,48 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        decimal novo_salario, ajuste;
+
+        Console.Write("Informe o salario do funcionario:");
+        string salar = Console.ReadLine();
+        Console.Write("Informe o código do cargo desse funcionario:");
+        string valor = Console.ReadLine();
+
+        int codigo = int.Parse(valor);
+        decimal salario = decimal.Parse(salar);
+
+        switch (codigo)
+        {
+            case 1:
+                novo_salario = (salario * 0.1M) + salario;
+                ajuste = salario * 0.1M;
+                break;
+
+            case 2:
+                novo_salario = (salario * 0.2M) + salario;
+                ajuste = salario * 0.2M;
+                break;
+
+            case 3:
+                novo_salario = (salario * 0.3M) + salario;
+                ajuste = salario * 0.3M;
+                break;
+
+            case 4:
+                novo_salario = (salario * 0.1M) + salario;
+                ajuste = salario * 0.1M;
+                break;
+
+            default:
+                novo_salario = (salario * 0.4M) + salario;
+                ajuste = salario * 0.4M;
+                break;
+        }
+
+        Console.WriteLine($"O novo salario eh de: {novo_salario}");
+        Console.WriteLine($"O antigo salario era de: {salario} ");
+        Console.WriteLine($"A diferença foi de: {ajuste}");
+
+    }
+}
---
+++ Ternario/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Informe um valor:");
+string auxValor = Console.ReadLine();
+int valor = int.Parse(auxValor);
+
+
+Console.WriteLine( (valor >= 10 ) ? "Maior/igual a dez!" : "Menor que dez!" );
