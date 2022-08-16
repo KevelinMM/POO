4f36a460045e03446b355f93e7d2a8b1e5e69841
Kevelin <kevelinmmattos@uniplaclages.edu.br>
2022-08-16 20:21:00 -0300

    breack e exercicios

---
+++ ExerForRestoIgual5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ExerForRestoIgual5
+{
+class Program
+    {
+        static void Main(string[] args)
+        {
+                for (short i = 2345; i >= 1287; i--)
+            {
+                if (i % 11 == 5)
+                 {
+                    Console.WriteLine(i);
+                 }
+            }
+
+        }
+    }
+}
---
+++ Unicode/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace UnicodeProblema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 33; i <= 126; i++)
+            {
+                Console.WriteLine("Caractere correspondente: ");
+            }
+        }
+    }
+}
