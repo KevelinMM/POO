ddd680e540b6f91cd861f70026a1b7aa16a808fd
Uniplac <github@uniplaclages.edu.br>
2022-10-11 14:28:54 -0300

    locadora de carros e exemplo de livros

---
+++ livrosExemplo/Program.cs
@@ -0,0 +1,56 @@
+﻿
+namespace IntroducaoPOO;
+
+internal class Program
+{
+    class Livro
+    {
+        public string Titulo;
+        public string SubTitulo;
+        public string Escritor;
+    }
+    class Leitor
+    {
+        public string Nome;
+        public byte Idade;
+        public List<Livro> LivrosLeitor = new List<Livro>();
+
+        public void AdiconarLivro(Livro livro)
+        {
+            this.LivrosLeitor.Add(livro);
+        }
+    }
+    private static void Main(string[] args)
+    {
+
+        Livro l1 = new Livro();
+        l1.Titulo = "O contador de Histórias";
+        l1.SubTitulo = "Memórias de vida e música";
+        l1.Escritor = "Dave Grohl";
+        Livro l2 = new Livro();
+        l2.Titulo = "O guia do Mochileiro das Galáxias";
+        l2.SubTitulo = "Não entre em pânico";
+        l2.Escritor = "Douglas Adams";
+        Livro l3 = new Livro();
+        l3.Titulo = "Memórias de um legionário";
+        l3.Escritor = "Dado Villa-Lobos";
+
+        Leitor leitor01 = new Leitor();
+        leitor01.Nome = "Luciano Coelho";
+        leitor01.Idade = 46;
+        leitor01.AdiconarLivro(l1);
+        leitor01.AdiconarLivro(l2);
+        leitor01.AdiconarLivro(l3);
+
+        Console.WriteLine($"{leitor01.Nome}, {leitor01.Idade} anos.");
+        Console.WriteLine($"Livros: ");
+        foreach (var livro in leitor01.LivrosLeitor)
+        {
+
+            Console.WriteLine($"{livro.Titulo}, {livro.Escritor}");
+
+        }
+    }
+
+
+}
---
+++ locadoraCarros/Program.cs
@@ -0,0 +1,56 @@
+﻿
+namespace IntroducaoPOO;
+
+internal class Program
+{
+    class Carro
+    {
+        public string Marca;
+        public string Placa;
+        public int Ano;
+        public string Estado;
+        public string Cor;
+    }
+    class Revenda
+    {
+        public string Nome;
+        public int AnoRevenda;
+        public List<Carro> CarrosRevenda = new List<Carro>();
+
+        public void AdiconarCarros(Carro carro)
+        {
+            this.CarrosRevenda.Add(carro);
+        }
+    }
+    private static void Main(string[] args)
+    {
+
+        Carro c1 = new Carro();
+        c1.Marca = "Peugeot";
+        c1.Placa = "ACDP";
+        c1.Ano = 2010;
+        c1.Estado = "Usado";
+        c1.Cor = "Prata";
+        Carro c2 = new Carro();
+        c2.Marca = "Honda";
+        c2.Placa = "RYB";
+        c2.Ano = 2022;
+        c2.Estado = "Novo";
+        c2.Cor = "Preto";
+
+        Revenda revenda01 = new Revenda();
+        revenda01.Nome = "Kevelin Mondadori";
+        revenda01.AnoRevenda = 2020;
+        revenda01.AdiconarCarros(c1);
+        revenda01.AdiconarCarros(c2);
+
+        Console.WriteLine($"Cliente: {revenda01.Nome} - Data da revenda: {revenda01.AnoRevenda}.");
+        Console.WriteLine($"Carros : ");
+        foreach (var carro in revenda01.CarrosRevenda)
+        {
+            Console.WriteLine($" \r\n Placa: {carro.Placa}, \r\n Marca: {carro.Marca}, \r\n Estado: {carro.Estado}, \r\n Ano: {carro.Ano}, \r\n Cor: {carro.Cor}");
+        }
+    }
+}
+
+//Kevelin Mondadori Mattos
